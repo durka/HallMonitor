5dfb2181c15532130f0309bedf8ac481c6fd9195
Alex Burka <aburka@seas.upenn.edu>
2013-12-31 13:22:00 -0500

    we require 4.3, so we shouldn’t need to set a deprecated setting from <3.0

--- src/org/durka/hallmonitor/CameraPreview.java
+++ src/org/durka/hallmonitor/CameraPreview.java
@@ -25,8 +25,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
