c55549f4570f523dfb366ee3a5c9336bf652816b
Alex Burka <aburka@seas.upenn.edu>
2014-06-29 15:08:32 -0400

    remove files that shouldn't have been committed

---
+++ cm_certs/build_signed_system.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# keytool-importkeypair -k cm_platform.keystore -p android -pk8 platform.pk8 -cert platform.x509.pem -alias platform
+APPNAME=HallMonitor
+MODE=debug
+ant clean
+ndk-build clean
+ndk-build NDK_APPLICATION_MK=Application.mk
+ant $MODE
+if [ $MODE == release ]; then
+    java -jar cm_certs/{signapk.jar,platform.{x509.pem,pk8}} bin/$APPNAME-$MODE-un{s,al}igned.apk
+    zipalign -v 4 bin/$APPNAME-$MODE{-unaligned,}.apk
+fi
+
